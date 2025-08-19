134dfe20d8bd7b21a2cd6b96ca411ff2f00262a9
Jonatas de Lima <delimajonatas875@gmail.com>
2025-08-19 20:27:42 -0300

    chore: delete build directory

--- build/CMakeFiles/tests.dir/test/suite_test.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/tests.dir/test/suite_test.c.o: \
- /home/jonatasroot/Documentos/workspace/cpp/clang/gbemu/test/suite_test.c \
- /usr/include/stdc-predef.h \
- /home/jonatasroot/Documentos/workspace/cpp/clang/gbemu/include/gbemutest/core/instructions_test.h
