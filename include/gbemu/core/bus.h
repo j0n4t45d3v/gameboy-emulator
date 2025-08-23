750179f824ca9337296971593600bc19a47d79af
Jonatas <delimajonatas875@gmail.com>
2025-08-23 11:15:12 -0300

    chore: implement others addrs in bus

--- include/gbemu/core/bus.h
+++ include/gbemu/core/bus.h
@@ -4,9 +4,49 @@
 #include "gbemu/external/cartridger.h"
 #include <stdint.h>
 
+#define IS_BANK00(addr) addr >= 0x0000 && addr <= 0x3FFF
+
+#define START_ADDR_BANK01_NN 0x4000
+#define END_ADDR_BANK01_NN 0x7FFF
+#define IS_BANK01_NN(addr) addr >= START_ADDR_BANK01_NN && addr <= END_ADDR_BANK01_NN
+
+#define START_ADDR_VIDEO_RAM 0x8000
+#define END_ADDR_VIDEO_RAM 0x9FFF
+#define IS_VIDEO_RAM(addr) addr >= START_ADDR_VIDEO_RAM && addr <= END_ADDR_VIDEO_RAM
+
+#define START_ADDR_EXTERNAL_RAM 0xA000
+#define END_ADDR_EXTERNAL_RAM 0xBFFF
+#define IS_EXTERNAL_RAM(addr) addr >= START_ADDR_EXTERNAL_RAM && addr <= END_ADDR_EXTERNAL_RAM
+
+#define START_ADDR_WORK_RAM0 0xC000
+#define END_ADDR_WORK_RAM0 0xCFFF
+#define IS_WORK_RAM0(addr) addr >= START_ADDR_WORK_RAM0 && addr <= END_ADDR_WORK_RAM0
+
+#define START_ADDR_WORK_RAM1 0xD000
+#define END_ADDR_WORK_RAM1 0xDFFF
+#define IS_WORK_RAM1(addr) addr >= START_ADDR_WORK_RAM1 && addr <= END_ADDR_WORK_RAM1
+
+#define START_ADDR_OBJECT_ATTR_MEMORY 0xFE00
+#define END_ADDR_OBJECT_ATTR_MEMORY 0xFE9F
+#define IS_OBJECT_ATTR_MEMORY(addr) addr >= START_ADDR_OBJECT_ATTR_MEMORY && addr <= END_ADDR_OBJECT_ATTR_MEMORY
+
+#define START_ADDR_IO_REGISTERS 0xFF00
+#define END_ADDR_IO_REGISTERS 0xFFFE
+#define IS_IO_REGISTERS(addr) addr >= START_ADDR_IO_REGISTERS && addr <= END_ADDR_IO_REGISTERS
+
+#define START_END_ADDR_INTERRUPT_ENABLE_REGISTER 0xFFFF
+#define IS_INTERRUPT_ENABLE_REGISTER(addr) addr == START_END_ADDR_INTERRUPT_ENABLE_REGISTER
+
 typedef struct {
   cartridger_t* cart;
+  uint8_t vram[END_ADDR_VIDEO_RAM - START_ADDR_VIDEO_RAM];
+  uint8_t wram0[END_ADDR_WORK_RAM0 - START_ADDR_WORK_RAM0];
+  uint8_t wram1[END_ADDR_WORK_RAM1 - START_ADDR_WORK_RAM1];
+  uint8_t oam[END_ADDR_OBJECT_ATTR_MEMORY - START_ADDR_OBJECT_ATTR_MEMORY];
+  uint8_t io_register[END_ADDR_IO_REGISTERS - START_ADDR_IO_REGISTERS];
+  uint8_t ie_register;
 } bus_t;
+
 
 uint8_t read_bus(bus_t*, uint16_t); 
 uint8_t write_bus(bus_t*, uint16_t, uint8_t); 
--- src/core/bus.c
+++ src/core/bus.c
@@ -1,21 +1,35 @@
 #include "gbemu/core/bus.h"
-#include "gbemu/common/logger.h"
 
-#define IS_BANK00(addr) addr >= 0x0000 && addr <= 0x3FFF
-#define IS_BANK01_NN(addr) addr >= 0x4000 && addr <= 0x7FFF
-#define IS_EXTERNAL_RAM(addr) addr >= 0xA000 && addr <= 0xBFFF
 
 uint8_t read_bus(bus_t *busAddr, uint16_t addr) {
   if (IS_BANK00(addr)) {
     return (uint8_t)busAddr->cart->bank00[addr];
   }
   if (IS_BANK01_NN(addr)) {
-    return (uint8_t)busAddr->cart->bank01NN[addr];
+    return (uint8_t)busAddr->cart->bank01NN[addr - START_ADDR_BANK01_NN];
+  }
+  if (IS_VIDEO_RAM(addr)) {
+    return busAddr->vram[addr - START_ADDR_VIDEO_RAM];
   }
   if (IS_EXTERNAL_RAM(addr)) {
-    LOG_DEBUG("TESTE");
-    return busAddr->cart->ram[addr];
+    return busAddr->cart->ram[addr - START_ADDR_EXTERNAL_RAM];
   }
+  if (IS_WORK_RAM0(addr)) {
+    return busAddr->wram0[addr - START_ADDR_WORK_RAM0];
+  }
+  if (IS_WORK_RAM1(addr)) {
+    return busAddr->wram1[addr - START_ADDR_WORK_RAM1];
+  }
+  if (IS_OBJECT_ATTR_MEMORY(addr)) {
+    return busAddr->oam[addr - START_ADDR_OBJECT_ATTR_MEMORY];
+  }
+  if (IS_IO_REGISTERS(addr)) {
+    return busAddr->oam[addr - START_ADDR_IO_REGISTERS];
+  }
+  if (IS_INTERRUPT_ENABLE_REGISTER(addr)) {
+    return busAddr->oam[addr - START_END_ADDR_INTERRUPT_ENABLE_REGISTER];
+  }
+
   return -1;
 }
 
@@ -24,10 +38,28 @@ uint8_t write_bus(bus_t *busAddr, uint16_t addr, uint8_t value) {
     busAddr->cart->bank00[addr] = value;
   }
   if (IS_BANK01_NN(addr)) {
-    busAddr->cart->bank01NN[addr] = value;
+    busAddr->cart->bank01NN[addr - START_ADDR_BANK01_NN] = value;
+  }
+  if (IS_VIDEO_RAM(addr)) {
+    busAddr->vram[addr - START_ADDR_VIDEO_RAM] = value;
   }
   if (IS_EXTERNAL_RAM(addr)) {
     busAddr->cart->ram[addr] = value;
+  }
+  if (IS_WORK_RAM0(addr)) {
+    busAddr->wram0[addr - START_ADDR_WORK_RAM0] = value;
+  }
+  if (IS_WORK_RAM1(addr)) {
+    busAddr->wram1[addr - START_ADDR_WORK_RAM1] = value;
+  }
+  if (IS_OBJECT_ATTR_MEMORY(addr)) {
+    busAddr->oam[addr - START_ADDR_OBJECT_ATTR_MEMORY] = value;
+  }
+  if (IS_IO_REGISTERS(addr)) {
+    busAddr->oam[addr - START_ADDR_IO_REGISTERS] = value;
+  }
+  if (IS_INTERRUPT_ENABLE_REGISTER(addr)) {
+    busAddr->oam[addr - START_END_ADDR_INTERRUPT_ENABLE_REGISTER] = value;
   }
   return 1;
 }
