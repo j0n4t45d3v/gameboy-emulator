f5f300f9101debb27409f1bfc1821abc4310a82a
Jonatas <delimajonatas875@gmail.com>
2025-08-26 20:17:55 -0300

    feat: run individual test

--- test/suite_test.c
+++ test/suite_test.c
@@ -1,6 +1,26 @@
+#include "gbemutest/core/bus_test.h"
 #include "gbemutest/core/instructions_test.h"
+#include "gbemutest/external/cartridger_test.h"
+#include <stdio.h>
+#include <string.h>
+
 
 int main(int argc, char *argv[]) {
+  if (argc == 3 && strcmp("--suite", argv[1]) == 0) {
+    if (strcmp("instruction", argv[2])) {
+      run_instructions_suite();
+    } else if (strcmp("bus", argv[2])) {
+      run_bus_suite();
+    } else if (strcmp("cartridger", argv[2])) {
+      run_cartridger_suite();
+    } else {
+      printf("Invalid Suite!\n");
+      return 1;
+    }
+    return 0;
+  }
   run_instructions_suite();
+  run_bus_suite();
+  run_cartridger_suite();
   return 0;
 }
