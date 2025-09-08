223f645d85e7b9ddee632fdea024b25cead87eb6
Jonatas <delimajonatas875@gmail.com>
2025-09-08 20:06:19 -0300

    chore: implementation byte rotation

--- include/gbemu/core/alu.h
+++ include/gbemu/core/alu.h
@@ -3,14 +3,24 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define LAST_BITE(byte) (byte & 0x01) 
+#define FIRST_BITE(byte) (byte & 0b10000000) >> 7 
+
 typedef struct {
   uint8_t result;
   bool has_carry;
   bool has_half_carry;
 } alu_result_t; 
 
+//ARITHMETIC
 alu_result_t alu_add(uint8_t input1, uint8_t input2); 
 alu_result_t alu_sub(uint8_t input1, uint8_t input2); 
 alu_result_t alu_and(uint8_t input1, uint8_t input2); 
 alu_result_t alu_or(uint8_t input1, uint8_t input2); 
 alu_result_t alu_xor(uint8_t input1, uint8_t input2); 
+
+//ROTATION
+alu_result_t alu_rotate_left(uint8_t input); 
+alu_result_t alu_rotate_left_circular(uint8_t input); 
+alu_result_t alu_rotate_right(uint8_t input); 
+alu_result_t alu_rotate_right_circular(uint8_t input); 
--- src/core/alu.c
+++ src/core/alu.c
@@ -43,3 +43,40 @@ alu_result_t alu_xor(uint8_t input1, uint8_t input2) {
   alu_result.has_half_carry = false;
   return alu_result;
 }
+
+/*           ROTATION AND SHIFT            */
+alu_result_t alu_rotate_left(uint8_t input) {
+  alu_result_t alu_result;
+  uint8_t first_bite = FIRST_BITE(input);
+  alu_result.result = (input << 1);
+  alu_result.has_carry = first_bite == 1;
+  alu_result.has_half_carry = false;
+  return alu_result;
+} 
+
+alu_result_t alu_rotate_left_circular(uint8_t input) {
+  alu_result_t alu_result;
+  uint8_t first_bite = FIRST_BITE(input);
+  alu_result.result = (input << 1) | first_bite;
+  alu_result.has_carry = first_bite == 1;
+  alu_result.has_half_carry = false;
+  return alu_result;
+}
+
+alu_result_t alu_rotate_right(uint8_t input) {
+  alu_result_t alu_result;
+  uint8_t last_bite = LAST_BITE(input);
+  alu_result.result = (input >> 1)  ;
+  alu_result.has_carry = last_bite == 1;
+  alu_result.has_half_carry = false;
+  return alu_result;
+} 
+
+alu_result_t alu_rotate_right_circular(uint8_t input) {
+  alu_result_t alu_result;
+  uint8_t last_bite = LAST_BITE(input);
+  alu_result.result = (last_bite << 7) | (input >> 1)  ;
+  alu_result.has_carry = last_bite == 1;
+  alu_result.has_half_carry = false;
+  return alu_result;
+}
